76c69593076043088031646233b877fa8284daac
Anthony D'Andrea <anthonydandrea2@gmail.com>
2017-11-03 10:22:11 +0100

    Added java examples

---
+++ java-examples/Swing example/src/imageviewer/FileImage.java
@@ -0,0 +1,39 @@
+package imageviewer;
+
+import java.io.*;
+
+public class FileImage {
+	private byte[] _data;
+	private String _path;
+	
+	
+	public FileImage(String filepath) {
+		_path = filepath;
+		_data = load(_path);
+	}
+	
+	public byte[] getData() {
+		return _data;
+	}
+	
+	public String getPath() {
+		return _path;
+	}
+	
+	private byte[] load(String path) {
+		try {
+			File f = new File(path);
+			int size = (int)f.length();
+			byte[] contents = new byte[size];
+			FileInputStream fin = new FileInputStream(f);
+			int n = 0;
+			while (n < size) {
+				n += fin.read(contents, n, size-n);
+			}
+			fin.close();
+			return contents;
+		} catch (IOException e) {
+			throw new Error(e.getMessage());
+		}
+	}
+}
---
+++ java-examples/Swing example/src/imageviewer/TickThread.java
@@ -0,0 +1,20 @@
+package imageviewer;
+
+public class TickThread extends Thread {
+	private Monitor _monitor;
+	
+	public TickThread(Monitor m) {
+		_monitor = m;
+	}
+	
+	public void run() {
+		try {
+			while (true) {
+				Thread.sleep(1000);
+				_monitor.tick();
+			}
+		} catch (InterruptedException e) {
+			// Exit
+		}
+	}
+}
